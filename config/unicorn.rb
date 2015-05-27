a8dd1483db04e4ea13077635195c6630de060223
slojo404 <slojo404@yahoo.com>
2015-05-26 21:29:04 -0400

    Update unicorn.rb
    temp mods working through ansible test

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
 worker_processes 2 # this should be >= nr_cpus
-pid "/usr/local/freeswitch/webapp/log/unicorn.pid"
+pid "/var/run/ostnapp/ostnapp.pid"
 stderr_path "/usr/local/freeswitch/webapp/log/unicorn.log"
 stdout_path "/usr/local/freeswitch/webapp/log/unicorn.log"
